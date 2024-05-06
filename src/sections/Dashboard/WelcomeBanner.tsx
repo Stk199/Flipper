9a2704e608b0f9faeb9e626ac81ac30c583d1568
Stk199 <168918083+Stk199@users.noreply.github.com>
2024-05-07 00:23:53 +0200

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -24,7 +24,7 @@ const Buttons = styled.div`
     padding: 10px;
     background: #ffffffdf;
     transition: background .2s ease;
-     color: black;
+     color: white;
     cursor: pointer;
     &:hover {
       background: white;
@@ -36,6 +36,7 @@ const Welcome = styled.div`
   background: url('banner.jpg') no-repeat center center;
   background-size: cover;
   border-radius: 10px;
+  color: white;
   position: relative;
   display: flex;
   flex-direction: column; // Changed to column to stack elements vertically
